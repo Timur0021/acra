eef1e3d7b7dd7c2c6557f6a8caec3ef5de092e0a
tima <tima.stella88@gmail.com>
2024-06-19 20:45:16 +0300

    -

---
+++ database/migrations/2024_06_19_173604_create_workers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('last_name')->nullable();
+            $table->string('phone')->unique();
+            $table->foreignId('service_id')->nullable()->constrained('services');
+            $table->decimal('salary', 10, 2)->default(0);
+            $table->date('birthday')->nullable();
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workers');
+    }
+};
